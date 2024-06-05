3f27fbce7ec31867ddf6ea9458f33cd508cfc708
Alejandro Cuartas Marin <cuartasmarin734@gmail.com>
2024-06-05 09:22:17 -0500

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getEnv } from './app.environment';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: getEnv('DB_HOST'),
+      port: Number(getEnv('DB_PORT')),
+      password: getEnv('DB_PASSWORD'),
+      username: getEnv('DB_USERNAME'),
+      database: getEnv('DB_NAME'),
+      entities: [
+        __dirname + '/users/entities/*.entity{.ts,.js}',
+      ],
+      synchronize: true,
+      logging: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }),
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import { config } from 'dotenv';
+config();
+
+import { ValidationPipe, BadRequestException } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (errors) => {
+        const result = errors.map((error) => ({
+          property: error.property,
+          message: error.constraints[Object.keys(error.constraints)[0]],
+        }));
+        return new BadRequestException({ validation_errors: result });
+      },
+      stopAtFirstError: true,
+    }),
+  );
+
+  app.enableCors({ origin: '*' });
+
+  const portStr = process.env.PORT;
+  let port = Number(portStr);
+  if (!portStr || isNaN(port)) {
+    port = 8080;
+  }
+
+  await app.listen(Number(port));
+}
+bootstrap();
